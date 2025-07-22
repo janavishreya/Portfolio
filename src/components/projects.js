f74d50972034504a3da5c2987427f4b6bf3a7bb0
janavishreya <sheryarajesh28@gmail.com>
2025-07-23 00:15:23 +0530

    Update projects.js

--- src/components/projects.js
+++ src/components/projects.js
@@ -15,7 +15,7 @@ export default function Projects() {
           setIsVisible(true)
         }
       },
-      { threshold: 0.3 },
+      { threshold: 0.1 },
     )
 
     if (sectionRef.current) {
@@ -117,7 +117,7 @@ export default function Projects() {
           </div>
 
           {/* Projects Grid */}
-          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {filteredProjects.map((project, index) => (
               <ProjectCard key={project.id} project={project} index={index} />
             ))}
